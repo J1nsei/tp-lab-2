49496ffd8bbc9f5930e22ed74769ee114945d4f6
J1nsei <sergey5213@gmail.com>
2020-02-26 13:38:18 +0300

    msort

--- src/main1.cpp
+++ src/main1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 	const int n = 9;
 	int array[n] = { 1,-5,10,5,99,1500,900,321,0 };
-	merge(array, n);
+	msort(array, n);
 	for (int i = 0; i < n; i++) {
 		cout << array[i] << " ";
 	}
